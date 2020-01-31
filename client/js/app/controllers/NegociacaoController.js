25805ddc145a0778b4157d118e9c15f7a4aceab9
Ivan Oliveira <oliveira_ivan@ymail.com>
2020-01-31 16:00:56 -0300

    Resolvendo um problema com o paradigma funcional

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,23 @@
 class NegociacaoController {
     constructor() {
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event) {
         event.preventDefault();
+        let data = new Date(
+            ...this._inputData.value.split('-')
+            .map(function(item, indice) {
+                if(indice == 1) { // 1 é a posição do array criado pelo split
+                    return item -1;
+                }
+                return item;
+            })
+        );
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        console.log(data);
     }
 }
