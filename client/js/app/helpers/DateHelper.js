e51046cfb69ddd07ad8e6a385c38873a7a593c75
Ivan Oliveira <ivan.oliveira@cfp.org.br>
2020-03-09 15:39:49 -0300

    Template strings

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,19 +5,23 @@ class DateHelper {
     }
     
     static textoParaData(texto) {
-        return new Date(
-            ...texto
-            .split('-') // com o Spread operator isso gera um array
-            .map((item, indice) => {
-                if(indice == 1) { // 1 é a posição do array criado pelo split
-                    return item - 1;
-                }
-                return item;
-            })
-        );
+        if ( /\d{4}-\d{2}-\d{2}/.test(texto) ) {
+            return new Date(
+                ...texto
+                .split('-') // com o Spread operator isso gera um array
+                .map((item, indice) => {
+                    if(indice == 1) { // 1 é a posição do array criado pelo split
+                        return item - 1;
+                    }
+                    return item;
+                })
+            );
+        } else {
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        }
     }
 
     static dataParaTexto(data) {
-        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 }
