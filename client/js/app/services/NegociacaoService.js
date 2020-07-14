7ea084473bfaac8c5c8ff4222b016ffa4613de35
Ivan Oliveira <oliveira_ivan@ymail.com>
2020-07-14 12:11:00 -0300

    O problema da vida assíncrona

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -34,4 +34,76 @@ class NegociacaoService {
         // executa
         xhr.send();
     }
+
+    obterNegociacoesDaSemanaAnterior(cb) {
+        let xhr = new XMLHttpRequest();
+
+        // abre a conexão
+        xhr.open('GET', 'negociacoes/anterior');
+
+        /**
+         * Configurações
+         * 
+         * Estados da requisição:
+         * 
+         * 0: requisição ainda não iniciada
+         * 1: conexão com o servidor estabelecida
+         * 2: requisição recebida
+         * 3: processando requisição
+         * 4: requisição está concluída e a resposta está pronta
+         */
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    cb(
+                        null, 
+                        JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    );
+                } else {
+                    console.log(xhr.responseText);
+
+                    cb('Não foi possível obter as negociações da semana', null);
+                }
+            }
+        };
+
+        // executa
+        xhr.send();
+    }
+
+    obterNegociacoesDaSemanaRetrasada(cb) {
+        let xhr = new XMLHttpRequest();
+
+        // abre a conexão
+        xhr.open('GET', 'negociacoes/retrasada');
+
+        /**
+         * Configurações
+         * 
+         * Estados da requisição:
+         * 
+         * 0: requisição ainda não iniciada
+         * 1: conexão com o servidor estabelecida
+         * 2: requisição recebida
+         * 3: processando requisição
+         * 4: requisição está concluída e a resposta está pronta
+         */
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    cb(
+                        null, 
+                        JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    );
+                } else {
+                    console.log(xhr.responseText);
+
+                    cb('Não foi possível obter as negociações da semana', null);
+                }
+            }
+        };
+
+        // executa
+        xhr.send();
+    }
 }
